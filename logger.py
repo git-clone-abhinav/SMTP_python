bed302f96b0848e7cd36d600de0b25f69a2e7f06
RaspberryPi <abhi.anu003@gmail.com>
2021-08-24 22:34:20 +0530

    logger implementation

--- logger.py
+++ logger.py
@@ -3,8 +3,10 @@ import csv
 
 def log_it(id,message):
     
-    with open('/home/pi/Desktop/mail_logs.txt', 'w') as f:
-        writer = csv.writer()
-        log_line = f"{time.ctime} - {id} {message}".format()
-        writer.writerow(log_line)
+    with open('/home/pi/Desktop/mailLogs.txt', 'a') as f:
+        f.write(f"{time.ctime()} - {id} {message}\n".format())
         f.close()
+        
+# log_it(23123,"one")
+# log_it(23123,"two")
+# log_it(23123,"three")
